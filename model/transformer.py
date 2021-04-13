3e6e0b2566e21185390417cda103ac725c74ad63
panic <saitros2@gmail.com>
2021-04-14 01:46:04 +0900

    upload

---
+++ model/transformer.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+
+import numpy as np
+
+import time
+import os
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, fast_text_embedding, depth)
+        """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, fast_text_embedding, d_model)
+        k = self.wk(k)  
+        v = self.wv(v)  
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, fast_text_embedding_q, depth)
+        k = self.split_heads(k, batch_size)  
+        v = self.split_heads(v, batch_size)  
+
+
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  
+
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.d_model))  
+
+        output = self.dense(concat_attention)  
+
+        return output, attention_weights
+    
+def point_wise_feed_forward_network(d_model, dff):
+    return tf.keras.Sequential([
+      tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+      tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+    ])
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+        
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+        
+        self.layernorm_1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm_2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        
+        self.dropout_1 = tf.keras.layers.Dropout(rate)
+        self.dropout_2 = tf.keras.layers.Dropout(rate)
+        
+    def call(self, x, training, mask):
+        
+        attn_output, _ = self.mha(x, x, x, mask) # (batch, fasttext_embedding_dim, d_model)
+        attn_output = self.dropout_1(attn_output, training=training)
+        out_1 = self.layernorm_1(x + attn_output)
+        
+        ffn_output = self.ffn(out_1)
+        ffn_output = self.dropout_2(ffn_output, training=training)
+        out_2 = self.layernorm_2(out_1 + ffn_output)
+        
+        return out_2
+    
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
+               maximum_position_encoding, rate=0.1):
+        super(Encoder, self).__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        
+     여기 부분 수정가능성 검토
+        self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding,
+                                                self.d_model)
+
+        self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate)
+                           for _ in range(num_layers)]
+
+        self.dropout = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+
+        seq_len = tf.shape(x)[1]
+
+        # adding embedding and position encoding.
+        x = self.embedding(x)  
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+
+    return x  
